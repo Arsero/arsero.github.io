cc122fb04848382fe5c64e2388f02dbc2761b0c5
Azzedine <54241944+Arsero@users.noreply.github.com>
2021-03-23 22:27:45 +0100

    Update data.js

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,15 +2,15 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Azzedine El Boujoufi', // e.g: 'Name | Developer'
+  lang: 'fr', // e.g: en, es, fr, jp
+  description: 'Bienvenue sur mon site !', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
   title: 'Oh bonjour! Mon nom est',
-  name: 'Azzedine.',
+  name: 'Azzedine',
   subtitle: 'Je suis développeur .Net / Javascript.',
   cta: 'En savoir plus',
 };
@@ -28,30 +28,39 @@ export const aboutData = {
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
+    img: 'acryptex.png',
+    title: 'Acryptex',
+    info:
+      'Acryptex est un gestionnaire de mot de passe, une application bureau multi-plateforme. \
+      Stockez les informations de vos comptes dans un fichier local crypté. \
+      Verrouillez/déverrouillez ce fichier en vous authentifiant avec votre compte maître.',
+    info2: 'Technologies utilisées : React, Redux, TypeScript & Electron.',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: 'https://github.com/Arsero/acryptex', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
+    img: 'citassist.png',
+    title: 'Citassist - Hackathon Citizens of Wallonia 2020',
+    info:
+      'Citassist est une solution globale de prise en charge \
+    de personnes à besoin(s) spécifique(s) lors de leur arrivée à l’hôpital, \
+    à l’aéroport, ou dans tout endroit où des personnes souffrant de \
+    déficiences motrices, auditives ou mentales ont besoin d’accompagnement.',
+    info2: 'Technologies utilisées : React, React Native, TypeScript & .Net Core.',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: 'https://github.com/wearespacey/citassist', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
+    img: 'stfa.png',
+    title: 'Stay The F*** Apex - Stay the F*** At Home Hackathon',
+    info:
+      'STFA est une plateforme permettant de rassembler des propriétaires \
+      d’imprimantes 3D et des créateurs de modèles 3D pour leur \
+      permettre de partager et loueur leurs ressources.',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: 'https://github.com/wearespacey/staythef-apex', // if no repo, the button will not show up
   },
 ];
 
