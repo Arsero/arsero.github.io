a410e0e71ea7f6fbc8d23e6e12fd3fb03f5ced28
Azzedine <54241944+Arsero@users.noreply.github.com>
2021-04-03 23:58:19 +0200

    Add project

--- src/html.js
+++ src/html.js
@@ -19,6 +19,10 @@ export default function HTML({
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta
+          name="description"
+          content="Bienvenue sur mon site! Mon nom est Azzedine et je suis développeur .Net / Javascript."
+        />
         {headComponents}
       </head>
       <body {...bodyAttributes}>
